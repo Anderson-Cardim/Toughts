84a11bb1194334704513d288eb1d82558a67fa7b
Anderson cardim <cardimanderson0@gmail.com>
2025-10-20 00:42:34 -0300

    Criado arquivo .env, e variáveis de ambiente

--- db/conn.js
+++ db/conn.js
@@ -1,15 +1,25 @@
-const { Sequelize } = require('sequelize');
+const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize('toughts2', 'root', '', {
-  host: 'localhost',
-  dialect: 'mysql',
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT || "mysql",
+  }
+);
+// const sequelize = new Sequelize("toughts2", "root", "", {
+//   host: "localhost",
+//   dialect: "mysql",
+// });
+console.log("process: ", process.env);
 
 try {
-    sequelize.authenticate();
-    console.log('Conectado com sucesso ao banco de dados!');
+  sequelize.authenticate();
+  console.log("Conectado com sucesso ao banco de dados!");
 } catch (error) {
-    console.error('Não foi possível conectar ao banco de dados:', error);
+  console.error("Não foi possível conectar ao banco de dados:", error);
 }
 
 module.exports = sequelize;
--- index.js
+++ index.js
@@ -1,75 +1,78 @@
-const express = require('express');
-const exphbs = require('express-handlebars');
-const session = require('express-session');
-const FileStore = require('session-file-store')(session);
-const flash = require('connect-flash');
-
+const express = require("express");
+const exphbs = require("express-handlebars");
+const session = require("express-session");
+const FileStore = require("session-file-store")(session);
+const flash = require("connect-flash");
+require("dotenv").config({ path: "./.env", override: true });
 const app = express();
 
-const conn = require('./db/conn');
+const conn = require("./db/conn");
 
-const Tought = require('./models/Tought');
-const User = require('./models/User');
-const toughtsRoutes = require('./routes/toughtsRoutes');
-const authRoutes = require('./routes/authRoutes');
+const Tought = require("./models/Tought");
+const User = require("./models/User");
+const toughtsRoutes = require("./routes/toughtsRoutes");
+const authRoutes = require("./routes/authRoutes");
 
+const ToughtsController = require("./controllers/ToughtsController");
+const AuthController = require("./controllers/AuthController");
+console.log("Conectando com:", {
+  dbUser: process.env.DB_USER,
+  dbDialect: process.env.DB_DIALECT,
+  dbName: process.env.DB_NAME,
+  dbPass: process.env.DB_PASS,
+  dbHost: process.env.DB_HOST,
+});
 
-const ToughtsController = require('./controllers/ToughtsController');
-const AuthController = require('./controllers/AuthController');
-
-app.engine('handlebars', exphbs.engine());
-app.set('view engine', 'handlebars');
+app.engine("handlebars", exphbs.engine());
+app.set("view engine", "handlebars");
 app.use(
-    express.urlencoded({ 
-        extended: true 
-    }));
+  express.urlencoded({
+    extended: true,
+  })
+);
 
 app.use(express.json());
 
 app.use(
-    session({
-        name: 'session',
-        secret: 'meu segredo',
-        resave: false,
-        saveUninitialized: false,
-        store: new FileStore({
-            logFn: function() {},
-            path: require('path').join(require('os').tmpdir(), 'sessions'),
-        }),
-        cookie: {
-            secure: false, 
-            maxAge: 360000, 
-            expires: new Date(Date.now() + 360000),
-            httpOnly: true,
-        },
+  session({
+    name: "session",
+    secret: "meu segredo",
+    resave: false,
+    saveUninitialized: false,
+    store: new FileStore({
+      logFn: function () {},
+      path: require("path").join(require("os").tmpdir(), "sessions"),
     }),
+    cookie: {
+      secure: false,
+      maxAge: 360000,
+      expires: new Date(Date.now() + 360000),
+      httpOnly: true,
+    },
+  })
 );
 
 app.use(flash());
 
-
-app.use(express.static('public'));
-
+app.use(express.static("public"));
 
 app.use((req, res, next) => {
-    if(req.session.userid) {
-        res.locals.session = req.session;
-    }
+  if (req.session.userid) {
+    res.locals.session = req.session;
+  }
 
-    next();
-})
+  next();
+});
 
+app.use("/toughts", toughtsRoutes);
+app.use("/", authRoutes);
 
-app.use('/toughts', toughtsRoutes);
-app.use('/', authRoutes);
-
-app.get('/', ToughtsController.showToughts);
-
+app.get("/", ToughtsController.showToughts);
 
 conn
-    // .sync({ force: true }) // Use { force: true } to drop tables and recreate them
-    .sync()
-    .then(() => {
-        app.listen(3000);
-    })
-    .catch((err) => console.log(err));
+  // .sync({ force: true }) // Use { force: true } to drop tables and recreate them
+  .sync()
+  .then(() => {
+    app.listen(3000);
+  })
+  .catch((err) => console.log(err));
